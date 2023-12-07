ff12abbac03baad6e9bac741882ff7162637b079
deykaushik <dey.kaushik123@gmail.com>
2023-12-08 01:50:55 +0530

    Animation changes

--- src/app/animation/zoom.ts
+++ src/app/animation/zoom.ts
@@ -3,10 +3,9 @@ import { animate, style, transition, trigger } from '@angular/animations';
 export const zoom = trigger('zoom', [
   transition('void => active', [
     style({ opacity: 0, transform: 'scale(0.8)' }),
-    animate('0.2s ease-in', style({ opacity: 1, transform: 'scale(1)' })),
-  ]),
-  transition('active => void', [
-    style({ opacity: 1, transform: 'scale(1)' }),
-    animate('0.2s ease-out', style({ opacity: 0, transform: 'scale(0.8)' })),
+    animate(
+      '0.2s cubic-bezier(0.4, 0, 0.2, 1)',
+      style({ opacity: 1, transform: 'scale(1)' })
+    ),
   ]),
 ]);
